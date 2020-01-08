a939941d6edff8fc8ff5c6c7abd1b25f137d1e05
BENHAMIDA AISSA <abenhami@localhost.localdomain>
2020-01-08 10:18:46 +0100

    Creation of the project

---
+++ src/main/java/fr/upem/aquarium/Aquarium/AquariumApplication.java
@@ -0,0 +1,13 @@
+package fr.upem.aquarium.Aquarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquariumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/upem/aquarium/Aquarium/AquariumApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.upem.aquarium.Aquarium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquariumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
